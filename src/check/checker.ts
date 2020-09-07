1a9f5552a49df29acd321452c75dc293ea8e257a
Lionel Gaillard <lionel.gaillard@inexto.com>
2020-09-07 17:03:35 +0200

    Build with webpack

--- src/check/checker.ts
+++ src/check/checker.ts
@@ -1,5 +1,4 @@
 import { EventEmitter } from 'events';
-import { tick } from '../async';
 import { File } from '../files';
 import { TranslationFile } from '../translations';
 
@@ -9,22 +8,18 @@ export class Checker extends EventEmitter {
     const keys = getAllKeys(translations);
 
     this.emit('checking', { keys, sources, translations });
-    await tick();
 
     for (const key of keys) {
       const source = findUsage(key, sources);
       if (source) {
         this.emit('used', { key, source });
-        await tick();
       } else {
         unused.push(key);
         this.emit('unused', { key, sources });
-        await tick();
       }
     }
 
     this.emit('checked', { unused });
-    await tick();
 
     return unused;
   }
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const { BannerPlugin } = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: './src/cli.ts',
+  output: {
+    path: __dirname,
+    filename: 'jsonl',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [{ test: /\.ts/, loader: 'ts-loader' }],
+  },
+  plugins: [new BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
+};
